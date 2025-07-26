2d4ffd38dffdb0e5838834acc4b5abecfd06bffc
rpmellow <rp.mellow.escapade@gmail.com>
2025-07-26 16:45:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
 from pypdf import PdfReader, PdfWriter
